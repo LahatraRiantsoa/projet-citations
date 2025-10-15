fd9127c9110917a10721c43be7d14840d8fffa74
Riantsoa Lahatra <92744938+LahatraRiantsoa@users.noreply.github.com>
2025-10-15 14:34:04 +0200

    RollBack

--- public/script.js
+++ public/script.js
@@ -1,14 +1,8 @@
-// async function chargerCitation() {
-//   const res = await fetch('https://zenquotes.io/api/random');
-//   const data = await res.json();
-//   document.getElementById('citation').innerText = `${data[0].q} — ${data[0].a}`;
-// }
-
-// document.getElementById('nouvelle').addEventListener('click', chargerCitation);
-// window.onload = chargerCitation;
-
 async function chargerCitation() {
-  const response = await fetch('/citation');
-  const data = await response.json();
+  const res = await fetch('https://zenquotes.io/api/random');
+  const data = await res.json();
   document.getElementById('citation').innerText = `${data[0].q} — ${data[0].a}`;
 }
+
+document.getElementById('nouvelle').addEventListener('click', chargerCitation);
+window.onload = chargerCitation;
