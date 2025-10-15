a5f8b1ef2934d79d804b2ae0ed793a253b61070f
Riantsoa Lahatra <92744938+LahatraRiantsoa@users.noreply.github.com>
2025-10-15 21:48:32 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 
 async function chargerCitation() {
-  const res = await fetch('http://localhost:3000/api/quote');
+  const res = await fetch('/api/quote');
   const data = await res.json();
   document.getElementById('citation').innerText = `${data[0].q} — ${data[0].a}`;
 }
