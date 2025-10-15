c334b2e30d25095f36244a17ff256aecf4fef7e6
Riantsoa Lahatra <92744938+LahatraRiantsoa@users.noreply.github.com>
2025-10-15 13:54:00 +0200

    Modif fetch

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const fetch = require(' node - fetch ');
+const fetch = require('node-fetch');
 
 async function getCitation() {
   const response = await fetch(' https :// zenquotes . io / api / random ');
