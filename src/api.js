690029d87e0f41907a698ccae227044811197d9c
Riantsoa Lahatra <92744938+LahatraRiantsoa@users.noreply.github.com>
2025-10-15 13:29:53 +0200

    Update api.js
    Correction problème de cors

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
+const fetch = require('node-fetch');
+
 async function getCitation() {
   const res = await fetch('https://zenquotes.io/api/random');
-  if (!res.ok) throw new Error('Erreur API: ' + res.status);
   const data = await res.json();
   return `${data[0].q} — ${data[0].a}`;
 }
+
 module.exports = getCitation;
